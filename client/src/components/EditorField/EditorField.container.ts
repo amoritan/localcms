0f569a03fca344b26006b2b723cd16a236984cda
Andres Moritan <amoritan@me.com>
2021-04-05 20:04:18 +0100

    EditorField refactor bug fixed

--- client/src/components/EditorField/EditorField.container.ts
+++ client/src/components/EditorField/EditorField.container.ts
@@ -31,48 +31,41 @@ export interface Props extends OwnProps {
 }
 
 const EditorFieldContainer = (ownProps: OwnProps): JSX.Element => {
-  const value = useSelector((state: State) => {
-    if (
-      ownProps.fromList &&
-      ownProps.listOccurrenceId &&
-      ownProps.listFieldId
-    ) {
-      const { blockId, fieldId, listOccurrenceId, listFieldId } = ownProps;
-      return getListFieldValue(
-        state,
-        blockId,
-        fieldId,
-        listOccurrenceId,
-        listFieldId
-      );
-    }
-
-    return getFieldValue(state, ownProps.blockId, ownProps.fieldId);
-  });
+  let value: string;
+  let onFieldUpdated: (value: string) => void;
 
   const dispatch = useDispatch();
-  const onFieldUpdated = () => {
-    if (
-      ownProps.fromList &&
-      ownProps.listOccurrenceId &&
-      ownProps.listFieldId
-    ) {
-      const { blockId, fieldId, listOccurrenceId, listFieldId } = ownProps;
-      return (value: string) =>
-        dispatch(
-          contentListFieldUpdated(
-            blockId,
-            fieldId,
-            listOccurrenceId,
-            listFieldId,
-            value
-          )
-        );
-    }
 
-    return (value: string) =>
-      dispatch(contentFieldUpdated(ownProps.blockId, ownProps.fieldId, value));
-  };
+  if (ownProps.fromList && ownProps.listOccurrenceId && ownProps.listFieldId) {
+    const { listOccurrenceId, listFieldId } = ownProps;
+    value = useSelector((state: State) =>
+      getListFieldValue(
+        state,
+        ownProps.blockId,
+        ownProps.fieldId,
+        listOccurrenceId,
+        listFieldId
+      )
+    );
+    onFieldUpdated = (newValue: string) =>
+      dispatch(
+        contentListFieldUpdated(
+          ownProps.blockId,
+          ownProps.fieldId,
+          listOccurrenceId,
+          listFieldId,
+          newValue
+        )
+      );
+  } else {
+    value = useSelector((state: State) =>
+      getFieldValue(state, ownProps.blockId, ownProps.fieldId)
+    );
+    onFieldUpdated = (newValue: string) =>
+      dispatch(
+        contentFieldUpdated(ownProps.blockId, ownProps.fieldId, newValue)
+      );
+  }
 
   return EditorField({
     ...ownProps,
